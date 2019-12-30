2bf2d9e39a3cafebb037898158c3dc5eaaa6abb1
Данил Суетин <suetin085@gmail.com>
2019-12-30 16:05:45 +0300

    First working version

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+FROM alpine
+
+ARG HBASE_VERSION=1.3.6
+ARG PHOENIX_VERSION=4.14.2-HBase-1.3
+ARG APACHE_MIRROR="http://apache-mirror.rbc.ru/pub/apache"
+
+
+# Env
+ENV LANG=en_US.utf8                                                                                   \
+    JAVA_HOME=/usr/lib/jvm/java-1.8-openjdk                                                           \
+    HBASE_HOME=/usr/local/hbase-$HBASE_VERSION                                                        \
+    PHOENIX_HOME=/usr/local/apache-phoenix-$PHOENIX_VERSION-bin                                       \
+    HBASE_CONF_DIR=/usr/local/hbase-$HBASE_VERSION/conf                                               \
+    PATH=$PATH:usr/lib/jvm/java-1.8-openjdk/bin:/usr/local/hbase-$HBASE_VERSION/bin:/usr/local/apache-phoenix-$PHOENIX_VERSION-bin/bin
+
+
+# Install
+COPY install.sh /tmp/
+
+RUN chmod 755 /tmp/install.sh && \
+    /tmp/install.sh
+
+
+# Conf
+COPY hbase-site.xml $HBASE_HOME/conf/
+
+EXPOSE 2181 16000 16010 16020 16030
+
+CMD hbase-daemon.sh start zookeeper     && \
+    hbase-daemon.sh start regionserver && \
+    hbase-daemon.sh foreground_start master
---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+set -ex
+
+
+# Pre
+apk add --no-cache --virtual \
+    wget                     \
+    tar                      \
+    gnupg                       
+apk add --no-cache \
+    procps         \
+    bash           \
+		python         \
+    openjdk8
+mkdir /tmp/phoenix-test /tmp/hbase /tmp/zookeeper
+
+
+# Install hbase
+wget -O /tmp/phoenix-test/hbase-keys https://dist.apache.org/repos/dist/release/hbase/KEYS
+gpg --import /tmp/phoenix-test/hbase-keys                             
+wget -P /tmp/phoenix-test "$APACHE_MIRROR/hbase/hbase-$HBASE_VERSION/hbase-$HBASE_VERSION-bin.tar.gz"
+wget -P /tmp/phoenix-test "https://www.apache.org/dist/hbase/hbase-$HBASE_VERSION/hbase-$HBASE_VERSION-bin.tar.gz.asc"
+gpg --verify /tmp/phoenix-test/hbase-$HBASE_VERSION-bin.tar.gz.asc
+tar zxf /tmp/phoenix-test/hbase-$HBASE_VERSION-bin.tar.gz -C /usr/local
+
+
+# Install phoenix
+wget -O /tmp/phoenix-test/phoenix-keys https://dist.apache.org/repos/dist/release/phoenix/KEYS
+gpg --import /tmp/phoenix-test/phoenix-keys
+wget -P /tmp/phoenix-test "$APACHE_MIRROR/phoenix/apache-phoenix-$PHOENIX_VERSION/bin/apache-phoenix-$PHOENIX_VERSION-bin.tar.gz"
+wget -P /tmp/phoenix-test "https://www.apache.org/dist/phoenix/apache-phoenix-$PHOENIX_VERSION/bin/apache-phoenix-$PHOENIX_VERSION-bin.tar.gz.asc"
+gpg --verify /tmp/phoenix-test/apache-phoenix-$PHOENIX_VERSION-bin.tar.gz.asc
+tar zxf /tmp/phoenix-test/apache-phoenix-$PHOENIX_VERSION-bin.tar.gz -C /usr/local
+cp $PHOENIX_HOME/phoenix-$PHOENIX_VERSION-server.jar $HBASE_HOME/lib/
+
+# Clean
+rm -r /tmp/phoenix-test
+apk del  \
+    wget \
+    tar  \
+    gnupg                   
+rm /tmp/install.sh
